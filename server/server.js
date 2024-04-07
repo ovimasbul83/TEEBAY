8628d744dab45c0b96f381b8fcadfdee0cd89768
ovimasbul83 <ovimasbul83@gmail.com>
2024-04-07 19:24:52 +0600

    Modify Server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const { ApolloServer, gql } = require("apollo-server-express");
 const { PrismaClient } = require("@prisma/client");
-
-const authRoutes = require("./routes/auth.routes");
 const dotenv = require("dotenv");
 
 const prisma = new PrismaClient();
@@ -14,23 +12,16 @@ const server = new ApolloServer({ typeDefs, resolvers });
 const app = express();
 
 dotenv.config();
-
-// Middleware to parse JSON bodies
 app.use(express.json());
 
 app.use(cors());
-// Define REST API routes
-
-app.use("/api/v1/auth", authRoutes);
 
 async function startApolloServer() {
-  // Start ApolloServer
   await server.start();
 
-  // Apply ApolloServer middleware to Express app
+
   server.applyMiddleware({ app });
 
-  // Start server
   const PORT = process.env.PORT || 4003;
   app.listen(PORT, () => {
     console.log(
